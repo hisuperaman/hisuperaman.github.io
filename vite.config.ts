97f1fbb05db4816d5c946b92f692c29756b2d2ae
Aman <hisuperamanx@gmail.com>
2026-09-13 12:24:57 +0530

    update deploy.yml to use pages artifact

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ import {defineConfig} from 'vite';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-    base: '/',
     plugins: [react(), tailwindcss()],
     resolve: {
         alias: {'@': path.resolve(__dirname, 'src')},
